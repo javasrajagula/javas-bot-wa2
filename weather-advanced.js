ac284cead6faf33aaa6ad34fc6e6f436f539cbc3
javasrajagula <dran74563@gmail.com>
2026-03-29 21:48:53 +0700

    Create weather-advanced.js file with WeatherDashboard class methods

---
+++ weather-advanced.js
@@ -0,0 +1,19 @@
+class WeatherDashboard {
+    getCurrentWeather(location) {
+        // logic to get current weather for the location
+    }
+
+    getForecast(location, days) {
+        // logic to get weather forecast for the next 'days' days
+    }
+
+    formatWeatherReport(weatherData) {
+        // logic to format the weather report from weatherData
+    }
+
+    generateAlerts(weatherData) {
+        // logic to generate alerts based on weather data
+    }
+}
+
+module.exports = WeatherDashboard;
